3b10473105b232fdd76f5d556b81c57d7fffd3c0
Satyam Rana <57563008+satyamranatc@users.noreply.github.com>
2024-06-05 16:38:04 +0530

    first commit

---
+++ Shop/Products/static/Product.js
@@ -0,0 +1,80 @@
+// Select the main element where the products will be displayed
+let Main = document.querySelector("main");
+
+// Select the dropdown element for filtering products
+let Filter = document.getElementById("FilterProducts");
+
+// Initialize an empty variable to store fetched product data
+let data = "";
+
+// Asynchronous function to fetch products from the API
+async function GetProducts() {
+  // Fetch the product data from the API
+  data = await fetch("http://127.0.0.1:8000/Products/List/");
+  
+  // Convert the response data to JSON format
+  data = await data.json();
+  data = data.Data.Products
+  console.log(data)
+
+  // Log each product category to the console (for debugging)
+  for (let i of data) {
+    console.log(i.category);
+  }
+
+  // Display all fetched products initially
+  Display(data);
+}
+
+// Add an event listener to the filter dropdown to handle changes
+Filter.addEventListener("change", (e) => {
+  // Initialize a variable to store the filtered data
+  let NewData;
+
+  // Filter products based on the selected value in the dropdown
+  if (e.target.value == "Fashion") {
+    NewData = data.filter(
+      (e) => e.category == "women's clothing" || e.category == "men's clothing"
+    );
+    console.log(NewData); // Log the filtered data to the console (for debugging)
+  }
+  else if(e.target.value == "All") {
+    NewData = data;
+    console.log(NewData); // Log all data to the console (for debugging)
+  }
+  else if(e.target.value == "Electronics") {
+    NewData = data.filter((e) => e.category == "electronics");
+    console.log(NewData); // Log the filtered data to the console (for debugging)
+  }
+  else if(e.target.value == "Jewelery") {
+    NewData = data.filter((e) => e.category == "jewelery");
+    console.log(NewData); // Log the filtered data to the console (for debugging)
+  }
+
+  // Display the filtered data
+  Display(NewData);
+});
+
+// Call the function to fetch and display products when the script runs
+GetProducts();
+
+// Function to display the product data on the web page
+function Display(data) {
+  // Clear the main element's current content
+  Main.innerHTML = "";
+
+  // Loop through each product and create its HTML representation
+  data.forEach((element) => {
+    Main.innerHTML += `
+      <div class="ProductCard">
+        <img src="${element.Image}" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h4 class="card-title">${element.Name}</h4>
+          <p class="card-text">${element.Description}</p>
+          <p class="card-text">${element.Price}</p>
+          <button>Buy Now</button>
+        </div>
+      </div>
+    `;
+  });
+}
